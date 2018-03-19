a64b9e7ef707e286a58b3ad238123e25e636c139
田邉 大介 <tanabe_daisuke@waku-2.com>
2018-03-19 18:41:22 +0900

    Add step1

--- src/step1/index.js
+++ src/step1/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+font-size: 1.5em;
+text-align: center;
+color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+padding: 4em;
+background: papayawhip;
+`;
 
 const Component = () => {
   return (
-    <div>Step1 Test</div>
+    <Wrapper>
+      <Title>Hello World, this is my first styled component!</Title>
+    </Wrapper>
   );
 };
 
